2749fa93203b39c2a8b9dd382be5ad713fa75016
SangRok Jung <sesilao@naver.com>
2022-06-12 22:56:16 +0900

    study 0612

--- JAVA/SangRok Java Project/src/hello0609/student/Student.java
+++ JAVA/SangRok Java Project/src/hello0609/student/Student.java
@@ -6,8 +6,10 @@ public class Student
 	protected String birth;		// ������� 
 	protected String gender;	// ����
 	protected int  	 grade;		// �г�
-	
-	//������
+
+
+
+
 	public Student()
 	{
 		
@@ -22,6 +24,41 @@ public class Student
 		
 	}
 	
+	// public static boolean getTermIndex(String _term){
+	// 	int term_index = 0;
+	// 	switch(_term){
+	// 		case "1-1": 
+	// 			term_index = 0;
+	// 		break;
+
+	// 		case "1-2": 
+	// 			term_index = 1;
+	// 		break;
+
+	// 		case "2-1": 
+	// 			term_index = 2;
+	// 		break;
+
+	// 		case "2-2": 
+	// 			term_index = 3;
+	// 		break;
+
+	// 		case "3-1": 
+	// 			term_index = 4;
+	// 		break;
+			
+	// 		case "3-2": 
+	// 			term_index = 5;
+	// 		break;
+
+	// 		default:
+	// 			term_index =-1;
+	// 		break;
+	// 	}
+
+	// 	// retrun term_index;
+	// }
+
 	public String getName() 
 	{
 		return name;
