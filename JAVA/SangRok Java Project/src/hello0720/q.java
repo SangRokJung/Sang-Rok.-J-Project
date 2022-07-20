787bd44ceaad6bb38e036fadfa0d83e288295ecd
SangRok Jung <sesilao@naver.com>
2022-07-20 21:55:34 +0900

    0720 study

--- JAVA/SangRok Java Project/src/hello0720/q.java
+++ JAVA/SangRok Java Project/src/hello0720/q.java
@@ -9,6 +9,22 @@ public class q {
         Queue q = new LinkedList(); //Q인터페이스의 구현
         Stack st = new Stack();
 
-        
+        st.push("0");
+        st.push("1");
+        st.push("2");
+
+        q.offer("0");
+        q.offer("1");
+        q.offer("2");
+
+        System.out.println("=Stack=");
+        while(!st.isEmpty()){
+            System.out.println(st.pop()); // Stack에서 요소 하나를 꺼내
+        }
+
+        System.out.println("=Queue");
+        while(!q.isEmpty()){
+            System.out.println(q.poll()); // Queue에서 요소 하나를 꺼내
+        }
     }
 }
