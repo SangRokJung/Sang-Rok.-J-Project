676e5624bc5aa3e5db1227799f5ee9b625cf2990
SangRok Jung <sesilao@naver.com>
2022-07-30 14:57:09 +0900

    0730 study

---
+++ JAVA/SangRok Java Project/src/hello0730/mapiter.java
@@ -0,0 +1,20 @@
+package hello0730;
+
+import java.util.*;
+
+public class mapiter {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<String, String>();
+
+        map.put("english", "hi");
+        map.put("korean", "안녕");
+
+        Iterator<String> iter = map.keySet().iterator();
+
+        while (iter.hasNext()) {
+            String s = iter.next();
+            System.out.println(map.get(s));
+        }
+
+    }
+}
